598dbaec01274ff22d923b2331a6b091a341e1b7
Cole Kelly <Cole.jkelly1@yahoo.com>
2021-05-03 09:35:34 -0400

    Commit changes

---
+++ Rental_project.py
@@ -0,0 +1,57 @@
+import math
+
+class Rental_Property():
+    #Attributes
+    def __init__(self, income, Expenses, Cash_Flow, RIO):
+        self.income = rental_income
+        self.expenses = rental_expenses
+        self.cash_flow = Cash_Flow
+
+    def income(self):
+        while True:
+            current_expenses = int(input("Please enter Total monthly income: "))
+
+        else:
+             #Ask the user how many months of rents they are adding
+             time = int(input("How many months of rent? "))
+            total = (time / 2,000) * self.cost_per_month
+            #Laundry
+            #Storage
+            #Other
+
+            #Show them their total
+
+    #Expenses Function
+    def expenses(self):
+        while True:
+        response = input("What Expense would you like to add? Taxes/Insurance/Utilities/HOA fees? ").lower()
+
+
+
+    #Cash Flow Function 
+    def Cash_Flow(self):
+        #Cashflow=Income-Expenses
+
+
+    #Driver Function 
+    def main():
+
+    while True:
+        ask = input("What would you like to do? You can say 'Add expenses', 'Add Income', 'Calculate Cash Flow', 'Calculate ROI', or 'quit': ").lower()
+        if ask == "quit":
+            break
+        if ask == "Add Expenses":
+            
+        if ask == "Add Income":
+            
+        if ask == "Calculate Cash Flow":
+            
+        if ask == "Calculate ROI":
+            
+
+
+
+
+
+
+
